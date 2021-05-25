977ed3131146795fad38c4c118abe4b51e1dba0e
RussellJi <17361564789@163.com>
2021-05-25 11:50:17 +0800

    Spring Boot demo

---
+++ readingList/src/main/java/com/jhh/readingList/ReadingListApplication.java
@@ -0,0 +1,16 @@
+package com.jhh.readingList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class ReadingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingListApplication.class, args);
+	}
+
+
+}
