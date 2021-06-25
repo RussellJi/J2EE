bf396f1c1b18c22a5e5c0311635bb9eb0b1c6cae
RussellJi <17361564789@163.com>
2021-06-25 11:41:47 +0800

    servlet

---
+++ tomcat/src/main/java/com/jhh/web/servlet/ServletDemo01.java
@@ -0,0 +1,39 @@
+package com.jhh.web.servlet;
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.Date;
+
+/*
+* servlet快速入门
+*
+* */
+public class ServletDemo01 implements Servlet {
+    Date date = new Date();
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println(date);
+        System.out.println("Hello Servlet!");
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println(date);
+        System.out.println("Hello Service!");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println(date);
+        System.out.println("Bye Servlet!");
+    }
+}
